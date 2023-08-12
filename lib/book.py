23bec30401f87af7c45e1451200727890483096b
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-08-12 17:19:38 -0400

    complete project

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count
+
+    def get_count(self):
+        return self._page_count
+
+    def set_count(self, page):
+        if page == int:
+            self._page = page
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    page_count = property(get_count, set_count,)
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self, brand, size, condition = "New"):
+        self.brand = brand
+        self._size = size
+        self._condition = condition
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if size == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        
+    def get_condition(self):
+        return self._condition
+
+    def set_condition(self, condition):
+        self._condition = condition
+
+    condition = property(get_condition, set_condition,)
+    size = property(get_size, set_size,)
+    
+            
